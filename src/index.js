4062a6f4e830f0e83f08d4f2c9233c79e7e1e5ef
Buh90 <83811198+Buh90@users.noreply.github.com>
2024-07-26 20:31:09 +0200

    Start project

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-import './style.css';
-import html from './index.html';
+import "./style.css";
+import html from "./index.html";
 
-const button = document.querySelector('button');
+const button = document.querySelector("button");
+const input = document.querySelector("input");
 
-button.addEventListener('click', () => getData());
+button.addEventListener("click", () => getData());
+// input.addEventListener("keypress", () => {
+//   console.log(input.value);
+// });
 
 async function getData() {
-  const response = await fetch(
-    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/turin?unitGroup=us&key=FCNR4DAMNNUUQ26MFULH3A79C&contentType=json',
-    {
-      method: 'GET',
-      headers: {},
-    }
-  );
-
-  const responseJSON = await response.json();
-
-  console.log(responseJSON.currentConditions.temp);
-
-  // .then((response) => {
-  //     console.log(response);
-  // })
-  // .catch((err) => {
-  //     console.error(err);
-  // });
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input.value}?unitGroup=metric&key=FCNR4DAMNNUUQ26MFULH3A79C&contentType=json`,
+      {
+        method: "GET",
+        headers: {},
+      }
+    );
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+/* Giorno completo:
+data tempmax tempmin tempmaxpercep umidità vento(dir, media, raffiche) alba tramonto icona
+
+Orari:
+orario tempmax tempmin tempmaxpercep umidità precip(prob) vento(dir, media, raffiche) icona condition
+*/
